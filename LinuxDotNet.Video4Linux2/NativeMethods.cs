9c91196777ee1deda331deff8b1f2768d64d5b0d
usausa <machi.pon@gmail.com>
2025-12-10 16:18:34 +0900

    Update

--- LinuxDotNet.Video4Linux2/NativeMethods.cs
+++ LinuxDotNet.Video4Linux2/NativeMethods.cs
@@ -116,7 +116,7 @@ internal static class NativeMethods
         public fixed byte bus_info[BusInfoSize];
         public uint version;
         public uint capabilities;
-        public fixed uint reserved[3];
+        public fixed uint reserved[4];
     }
 
     [StructLayout(LayoutKind.Sequential, Pack = 8)]
