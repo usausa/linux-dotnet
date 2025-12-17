224e77a24902906454e42c50899f165d31508eba
usausa <machi.pon@gmail.com>
2025-12-17 17:46:07 +0900

    Update

--- Example.InputEvent.ConsoleApp/Program.cs
+++ Example.InputEvent.ConsoleApp/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+#pragma warning disable CA1416
+// ReSharper disable ConvertIfStatementToConditionalTernaryExpression
+// ReSharper disable CommentTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable StringLiteralTypo
+// ReSharper disable UseObjectOrCollectionInitializer
+
+// lsusb
+//
+// sudo nano /etc/udev/rules.d/99-barcode.rules
+// SUBSYSTEM=="input", ATTRS{idVendor}=="1130", ATTRS{idProduct}=="0001", MODE="0666"
+//
+// sudo apt install evtest
+// sudo evtest
+//
+// USB-TMU-V3
+
+using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
+
+using LinuxDotNet.InputEvent;
+
+var rootCommand = new RootCommand("Input example");
+
+var listCommand = new Command("list", "List devices");
+listCommand.Handler = CommandHandler.Create(static () =>
+{
+    foreach (var device in EventDeviceInfo.GetDevices())
+    {
+        Console.WriteLine($"{device.Device,-18}  {device.VendorId}:{device.ProductId}  {device.Name}");
+    }
+});
+rootCommand.Add(listCommand);
+
+rootCommand.Invoke(args);
---
+++ LinuxDotNet.InputEvent/EventDeviceInfo.cs
@@ -0,0 +1,60 @@
+namespace LinuxDotNet.InputEvent;
+
+using System.Globalization;
+
+public sealed class EventDeviceInfo
+{
+    private const string InputDevicePath = "/sys/class/input/";
+
+    public string Device { get; set; }
+
+    public string Name { get; set; }
+
+    public int Number { get; set; }
+
+    public string VendorId { get; set; }
+
+    public string ProductId { get; set; }
+
+    internal EventDeviceInfo(string device, string name, int number, string vendorId, string productId)
+    {
+        Device = device;
+        Name = name;
+        Number = number;
+        VendorId = vendorId;
+        ProductId = productId;
+    }
+
+    public override string ToString()
+    {
+        return $"{Device}: {Name} (Vendor: {VendorId}, Product: {ProductId})";
+    }
+
+    public static IReadOnlyList<EventDeviceInfo> GetDevices()
+    {
+        if (!Directory.Exists(InputDevicePath))
+        {
+            return [];
+        }
+
+        var devices = new List<EventDeviceInfo>();
+        foreach (var name in Directory.GetDirectories(InputDevicePath).Select(Path.GetFileName).Where(static x => x?.StartsWith("event", StringComparison.Ordinal) ?? false).OrderBy(static x => Int32.Parse(x!.AsSpan()[5..], CultureInfo.InvariantCulture)))
+        {
+            var number = Int32.Parse(name.AsSpan()[5..], CultureInfo.InvariantCulture);
+
+            devices.Add(new EventDeviceInfo(
+                $"/dev/input/{name}",
+                ReadFileValue(Path.Combine(InputDevicePath, name!, "device", "name")),
+                number,
+                ReadFileValue(Path.Combine(InputDevicePath, name!, "device", "id", "vendor")),
+                ReadFileValue(Path.Combine(InputDevicePath, name!, "device", "id", "product"))));
+        }
+
+        return devices;
+
+        static string ReadFileValue(string path)
+        {
+            return File.Exists(path) ? File.ReadAllText(path).Trim() : string.Empty;
+        }
+    }
+}
